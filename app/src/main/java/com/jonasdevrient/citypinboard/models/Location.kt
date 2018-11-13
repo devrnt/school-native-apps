22290f4236f9bb4e78721310097fc3df18515f50
JonasDeVrient <jonas.devrient.y0029@student.hogent.be>
2018-11-13 16:18:28 +0100

    Update models to work with backend

--- app/src/main/java/com/jonasdevrient/citypinboard/models/Location.kt
+++ app/src/main/java/com/jonasdevrient/citypinboard/models/Location.kt
@@ -3,4 +3,6 @@ package com.jonasdevrient.citypinboard.models
 class Location(
         val longitude: Number,
         val latitude: Number
-)
+) {
+    constructor():this(1, 2)
+}
--- app/src/main/java/com/jonasdevrient/citypinboard/models/Pinboard.kt
+++ app/src/main/java/com/jonasdevrient/citypinboard/models/Pinboard.kt
@@ -1,43 +1,10 @@
 package com.jonasdevrient.citypinboard.models
 
-class Pinboard(
-        var id: Int,
+data class Pinboard(
+        var _id: String,
         var city: String,
-        var location: String,
-        var posts: MutableList<String> = mutableListOf<String>()
+        var location: Location,
+        var posts: MutableList<Post> = mutableListOf()
 ) {
     val amountOfPosts get() = posts.count()
-
-
-    fun amountOfPosts(): Int {
-        return posts.count()
-    }
-
-    fun addPost(post: String) {
-        posts.add(post)
-    }
-}
-
-object Supplier {
-    val pinboards = listOf<Pinboard>(
-            Pinboard(1, "Oudenaarde", "1", mutableListOf("post 1", "post 2")),
-            Pinboard(2, "Zingem", "2", mutableListOf()),
-            Pinboard(3, "Namen", "3", mutableListOf("post 1")),
-            Pinboard(4, "Gent", "4", mutableListOf()),
-            Pinboard(2, "Zingem", "2", mutableListOf()),
-            Pinboard(3, "Namen", "3", mutableListOf("post 1")),
-            Pinboard(4, "Gent", "4", mutableListOf()),
-            Pinboard(2, "Zingem", "2", mutableListOf()),
-            Pinboard(3, "Namen", "3", mutableListOf("post 1")),
-            Pinboard(4, "Gent", "4", mutableListOf()),
-            Pinboard(2, "Zingem", "2", mutableListOf()),
-            Pinboard(3, "Namen", "3", mutableListOf("post 1")),
-            Pinboard(4, "Gent", "4", mutableListOf()),
-            Pinboard(2, "Zingem", "2", mutableListOf()),
-            Pinboard(3, "Namen", "3", mutableListOf("post 1")),
-            Pinboard(4, "Gent", "4", mutableListOf()),
-            Pinboard(2, "Zingem", "2", mutableListOf()),
-            Pinboard(3, "Namen", "3", mutableListOf("post 1")),
-            Pinboard(4, "Gent", "4", mutableListOf())
-    )
 }
