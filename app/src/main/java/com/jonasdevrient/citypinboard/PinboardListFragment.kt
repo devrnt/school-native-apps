2473ffa9f124e147e5954fcfae6509691709ef83
JonasDeVrient <jonas.devrient.y0029@student.hogent.be>
2018-11-13 16:24:18 +0100

    Add pinboard list with pinboard item

---
+++ app/src/main/java/com/jonasdevrient/citypinboard/PinboardListFragment.kt
@@ -0,0 +1,65 @@
+package com.jonasdevrient.citypinboard
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jonasdevrient.citypinboard.adapters.PinboardsAdapter
+import com.jonasdevrient.citypinboard.models.Pinboard
+import com.jonasdevrient.citypinboard.repositories.PinboardAPI
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+
+class PinboardListFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
+    private lateinit var pinboards: List<Pinboard>
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment with the CityList theme
+        val view = inflater.inflate(R.layout.pinboard_list_fragment, container, false)
+
+
+        recyclerView = view.findViewById(R.id.recycler_view)
+        viewManager = LinearLayoutManager(activity)
+
+        loadPinboards()
+
+
+        return view
+    }
+
+    private fun loadPinboards() {
+        val call = PinboardAPI.repository
+                .getAll()
+
+        call.observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::handleResponse, this::handleError)
+    }
+
+    private fun handleResponse(pinboards: List<Pinboard>) {
+
+        print(pinboards)
+        viewAdapter = PinboardsAdapter(context, pinboards)
+
+        recyclerView.apply {
+            setHasFixedSize(true)
+
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+
+    }
+
+    private fun handleError(error: Throwable) {
+        print(error)
+    }
+
+}
