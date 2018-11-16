1f1d5bab7a722312a0a0f5833c63d61f416d9eea
JonasDeVrient <jonas.devrient.y0029@student.hogent.be>
2018-11-16 13:05:34 +0100

    Add documentation for repositories

--- app/src/main/java/com/jonasdevrient/citypinboard/repositories/GebruikerRepository.kt
+++ app/src/main/java/com/jonasdevrient/citypinboard/repositories/GebruikerRepository.kt
@@ -1,6 +1,7 @@
 package com.jonasdevrient.citypinboard.repositories
 
 import com.jonasdevrient.citypinboard.models.Gebruiker
+import com.jonasdevrient.citypinboard.repositories.GebruikerAPI.repository
 import com.jonasdevrient.citypinboard.responses.CheckGebruikersnaamResponse
 import com.jonasdevrient.citypinboard.responses.RegistreerResponse
 import io.reactivex.Observable
@@ -10,18 +11,36 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
 
+/**
+ * Retrofit interface with the necessary methods to call the endpoints of the backend
+ */
 interface GebruikerRepository {
+    /**
+     *  Logs a [gebruiker] in
+     *  @return an @see Observable of type @see RegistreerResponse with a token
+     */
     @POST("users/login")
     fun login(@Body gebruiker: Gebruiker): Observable<RegistreerResponse>
 
+    /**
+     *  Logs a [gebruiker] in
+     *  @return an @see Observable of type @see RegistreerResponse with a token
+     */
     @POST("users/register")
     fun registreer(@Body gebruiker: Gebruiker): Observable<RegistreerResponse>
 
+    /**
+     *  Checks if a username is already taken
+     *  @return an @see Observable of type @see CheckGebruikersnaamResponse with result 'ok' or 'alreadyexists'
+     */
     @POST("users/checkusername")
     fun checkGebruikersnaam(@Body gebruikersnaamResponse: CheckGebruikersnaamResponse): Observable<CheckGebruikersnaamResponse>
 
 }
 
+/**
+ * Static class (see java) with a public attribute [repository]
+ */
 object GebruikerAPI {
     // online url
     private val API_BASE_URL = "https://citypinboard.herokuapp.com/api/"
@@ -30,6 +49,9 @@ object GebruikerAPI {
     // private val API_BASE_URL = "http://192.168.0.229:3000/api/"
 
 
+    /**
+     * [repository] used to make the required calls in the class @see GebruikerRepository
+     */
     var repository = Retrofit
             .Builder()
             .baseUrl(API_BASE_URL)
--- app/src/main/java/com/jonasdevrient/citypinboard/repositories/PinboardRepository.kt
+++ app/src/main/java/com/jonasdevrient/citypinboard/repositories/PinboardRepository.kt
@@ -1,17 +1,29 @@
 package com.jonasdevrient.citypinboard.repositories
 
 import com.jonasdevrient.citypinboard.models.Pinboard
+import com.jonasdevrient.citypinboard.repositories.GebruikerAPI.repository
+import com.jonasdevrient.citypinboard.repositories.PinboardAPI.repository
 import io.reactivex.Observable
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 
+/**
+ * Retrofit interface with the necessary methods to call the endpoints of the backend
+ */
 interface PinboardRepository {
+    /**
+     *  Gets all the pinboards
+     *  @return an @see Observable of a list @see Pinboard with a token
+     */
     @GET("pinboards")
     fun getAll(): Observable<List<Pinboard>>
 }
 
+/**
+ * Static class (see java) with a public attribute [repository]
+ */
 object PinboardAPI {
     // online url
     private val API_BASE_URL = "https://citypinboard.herokuapp.com/api/"
@@ -19,7 +31,9 @@ object PinboardAPI {
     // local url
     // private val API_BASE_URL = "http://192.168.0.229:3000/api/"
 
-
+    /**
+     * [repository] used to make the required calls in the class @see PinboardRepository
+     */
     var repository = Retrofit
             .Builder()
             .baseUrl(API_BASE_URL)
